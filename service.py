6acf612f961fdafea51a4600ceab2ce3b11f8279
Mickey Barboi <mbarboi@lefthandrobotics.com>
2017-09-06 14:00:54 -0600

    sync commit

--- service.py
+++ service.py
@@ -25,7 +25,7 @@ class Application(dbus.service.Object):
         self.services = []
         dbus.service.Object.__init__(self, bus, self.path)
         self.add_service(HeartRateService(bus, 0))
-        self.add_service(RobotService(bus, 0))
+        self.add_service(RobotService(bus, 1))
 
     def get_path(self):
         return dbus.ObjectPath(self.path)
