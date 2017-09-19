12c034be9480e9a72b57d74ac65d5d81c976af38
Mickey Barboi <mbarboi@lefthandrobotics.com>
2017-09-19 15:16:05 -0600

    remote sync

--- main.py
+++ main.py
@@ -8,12 +8,19 @@ little notice of whats going on with the data passing through them, and intentio
 only expose a pair of channels (in and out) on each of their interfaces. The controller
 has in/out endpoints on its BLE channel with Mobile, and in/out endpoints on the serial 
 connection with the host. This program may have a few more. 
+
+You know, I don't think we need a heavily multithreaded implementation here. Its more correct, 
+but the technical risk is limited by the bandwidth that the 6 channels are using, which
+is both light and limited by the concurrent design of the communication patterns.
+
+Then again, because the serial connection blocks, maybe the threading implementation is safer.
 '''
 
 import serial
-import threading
+from threading import Thread
+from Queue import Queue
 
-# Constant
+# Constants
 SERIAL_PORT = '/dev/ttyAMA0'
 SERIAL_BAUDRATE = 115200
 SERIAL_TIMEOUT = 0.1
@@ -22,28 +29,39 @@ SERIAL_RTSCTS = 1
 
 class SerialConnection(object):
     '''
-    Simple wrapper around a serial connection that is threadsafe
+    Threaded and Queued wrapper around a Serial connection.
     '''
 
     def __init__(self, port):
         super(SerialConnection, self).__init__()
-        self._serial = serial.Serial(port=port,
-                                     baudrate=SERIAL_BAUDRATE,
-                                     timeout=SERIAL_TIMEOUT,
-                                     rtscts=SERIAL_RTSCTS)
+        self._serial = serial.Serial(port=port, baudrate=SERIAL_BAUDRATE, timeout=SERIAL_TIMEOUT, rtscts=SERIAL_RTSCTS)
 
-        self._lock = threading.Lock()
+        self.queue = Queue()
+        self.thread = None
+        # self.is_open = False
+
+    def open(self):
+        ''' This doesen't open the serial connection, it starts the thread that listens to it '''
+        self.thread = Thread(target=self.read)
+        self.thread.start()
+
+    def close(self):
+        ''' Kill the thread'''
+        pass
 
     def read(self):
-        '''
-        Reads a line from the serial connection. Blocks if another 
-        read() or write() is in progress
-        '''
-        with self._lock:
-            return self._serial.readline()
+        ''' Reads a line from the serial connection. Blocks on concurrent reads or writes'''
+        # with self._lock:
+        #     return self._serial.readline()
+
+        while self._serial.in_waiting:
+            x = self._serial.readline()
+            x = x.decode()
+            self.queue.put(x)
+            print(x)
 
     def write(self, msg):
-        ''' Write a message to the serial connection '''
+        ''' Write a message to the serial connection. Blocks on concurrent reads or writes'''
         msg += '\r'
 
         with self._lock:
@@ -52,26 +70,32 @@ class SerialConnection(object):
     def waiting(self):
         return self._serial.in_waiting
 
-
-conn = SerialConnection(SERIAL_PORT)
+    def blockspin(self):
+        while self.running:
+            msg = self.queue.get()
+            print("Have message: ", msg)
 
 
 def main():
-    first = True
+    conn = SerialConnection(SERIAL_PORT)
+    conn.open()
+    conn.blockspin()
 
-    while True:
-        beb = conn._serial.in_waiting
+    # first = True
 
-        while beb > 0:
-            print("Waiting with bytes: " + str(beb))
-            x = conn.read()
-            x = x.decode()
-            print(x)
+    # while True:
+    #     beb = conn._serial.in_waiting
 
-        if first:
-            print("First....")
-            conn.write("txpower\r")
-            first = False
+    #     while beb > 0:
+    #         print("Waiting with bytes: " + str(beb))
+    #         x = conn.read()
+    #         x = x.decode()
+    #         print(x)
+
+    #     if first:
+    #         print("First....")
+    #         conn.write("txpower\r")
+    #         first = False
 
 
 if __name__ == '__main__':
