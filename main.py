56eec032d229944feef85dc10cb1d575ea598074
Mickey Barboi <mbarboi@lefthandrobotics.com>
2017-09-20 10:43:05 -0600

    remote sync

--- main.py
+++ main.py
@@ -63,7 +63,9 @@ class SerialConnection(object):
     def write(self, msg):
         ''' Write bytes to the serial connection, adding a carriage return'''
         assert(len(msg) <= MAX_OUT_LEN)
-        self._serial.write(b'se ' + msg + b'\r')
+        msg = b'se ' + msg + b'\r'
+        self._serial.write(msg)
+        print("writing: " + msg)
 
     def spinwait(self):
         ''' Spins on the inbound queue, returning messages as they arrive '''
